bda623be5ff0523c176e34db16398d0d980f1bca
lab302-37 <lab302-37@labeng.local>
2019-01-11 17:41:11 +0700

    no.3 finished

--- lab3_3.cpp
+++ lab3_3.cpp
@@ -1,16 +1,14 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 int main(){
-    int i,n=6,m=19;
-    double sum=0;
-    i=n;
-    while(i<=m){
-        sum=sum+1/i;
-        i=i+1;
+    double n=2,m=69,sum;
+    while(n<=m){    
+        sum+=1/n;
+        n=n+1;
     }
 
-    cout << sum;
+    cout <<"sum is " << sum;
 
 return 0;
 }
